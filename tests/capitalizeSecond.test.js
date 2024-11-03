55827c0c35d42f80b5ff3c85ba5973e4bc408050
Kirill <pesterevkv2003@gmail.com>
2024-11-03 21:54:59 +0300

    add jest

--- tests/capitalizeSecond.test.js
+++ tests/capitalizeSecond.test.js
@@ -1,6 +1,7 @@
 import { strict as assert } from 'node:assert';
 import { capitalize } from "../src/capitalize.js";
 
-assert.equal(capitalize(''), '');
-assert.equal(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize('hello'), 'hello');
 
