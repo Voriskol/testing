e93792f600eb205c3a6361c2dec004b4664512e0
Kirill <pesterevkv2003@gmail.com>
2024-11-03 19:28:18 +0300

    11

---
+++ tests/capitalizeSecond.test.js
@@ -0,0 +1,6 @@
+import { strict as assert } from 'node:assert';
+import { capitalize } from "../src/capitalize.js";
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+
